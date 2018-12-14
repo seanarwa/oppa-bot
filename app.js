b86997c76d9fe1599d099d95c2f57269b3aa77d8
Sean Chang <seantc@uci.edu>
2018-12-14 04:12:51 -0800

    add rant feature

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ client.on('message', message => {
 			case "give"  : routeGive(message);  break;
 			case "take"  : routeTake(message); break;
 			// case "bet"   : routeBet(message); break;
+			case "rant"  : routeRant(message); break;
             default: routeCommandNotFound(message);
         }
     }
@@ -200,6 +201,22 @@ function routeBet(message) {
 	} catch (error) {
 		console.log(error);
 	}
+}
+
+function routeRant(message) {
+	message.channel.send("Nani the fuck did you just fucking iimasu about watashi," 
+	+ " you chiisai bitch desuka? Watashi’ll have anata know that watashi graduated " 
+	+ "top of my class in Nihongo 3, and watashi’ve been involved in iroirona Nihongo " 
+	+ "tutoring sessions, and watashi have over sanbyaku perfect test scores. Watashi am " 
+	+ "trained in kanji, and watashi is the top letter writer in all of southern California. " 
+	+ "Anata are nothing to watashi but just another weaboo. Watashi will korosu " 
+	+ "anata the fuck out with vocabulary the likes of which has never been mimasu’d " 
+	+ "before on this continent, mark watashino fucking words. Anata thinks anata can " 
+	+ "get away with hanashimasing that kuso to watashi over the intaaneto? Omou again, " 
+	+ "fucker. As we hanashimasu, watashi am contacting watashino secret netto of otakus " 
+	+ "across the USA, and anatano IP is being traced right now so you better junbishimasu " 
+	+ "for the ame, ujimushi. The ame that korosu’s the pathetic chiisai thing anata calls " 
+	+ "anatano life. You’re fucking shinimashita’d, akachan.");
 }
 
 function routeHelp(message) {
